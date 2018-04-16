5d825f63a2a993ec6d07796ad26d1741e8426cbd
Jacob <jacobedwardhaig@gmail.com>
2018-04-16 10:35:39 -0400

    Chapter 1-2

---
+++ Source Code/Chapter 02/GreatFun.java
@@ -0,0 +1,11 @@
+// This is another simple Java program.
+
+public class GreatFun     // new class
+{
+     public static void main(String[] args)
+     {
+          System.out.println("Programming is great fun");
+         // System.out.println("great fun!");
+     }
+}  // end class
+
